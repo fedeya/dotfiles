6a395c5eb502d512f504af74b338912b6f5c2a2c
Federico Minaya <fede@Federicos-MacBook-Air.local>
2024-03-09 23:28:31 -0300

    add install script

---
+++ scripts/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo "Installing Homebrew"
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+echo "Installing Homebrew Packages"
+
+brew install \
+  neovim \
+  ripgrep \
+  bat \
+  eza \
+  asdf \
+  zoxide \
+  fzf \
+
+echo "Installing Homebrew Casks"
+
+brew install --cask \
+  google-chrome \
+  spotify \
+  slack \
+  notion \
+  1password \
+  discord \
+  kitty \
+  raycast \
+
+echo "Installing Homebrew Fonts"
+
+brew tap homebrew/cask-fonts
+
+brew install --cask \
+  font-jetbrains-mono-nerd-font \
