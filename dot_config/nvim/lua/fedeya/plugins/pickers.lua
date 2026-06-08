fc43d7204c59606407d939e4240bd694f3abd9e3
Federico Minaya <hello@fedeminaya.com>
2026-06-08 00:49:50 -0300

    use sidekick

--- dot_config/nvim/lua/fedeya/plugins/pickers.lua
+++ dot_config/nvim/lua/fedeya/plugins/pickers.lua
@@ -385,8 +385,6 @@ return {
       -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
       require("fff.download").download_or_build_binary()
     end,
-    -- if you are using nixos
-    -- build = "nix run .#release",
     opts = {                -- (optional)
       debug = {
         enabled = false,    -- we expect your collaboration at least during the beta
@@ -394,8 +392,6 @@ return {
       },
       prompt = "",
     },
-    -- No need to lazy-load with lazy.nvim.
-    -- This plugin initializes itself lazily.
     lazy = false,
     keys = {
       {
@@ -408,44 +404,6 @@ return {
         function() require('fff').live_grep() end,
         desc = 'LiFFFe grep',
       },
-      -- {
-      --   "fz",
-      --   function()
-      --     require('fff').live_grep({
-      --       grep = {
-      --         modes = { 'fuzzy', 'plain' }
-      --       }
-      --     })
-      --   end,
-      --   desc = 'Live fffuzy grep',
-      -- },
-      -- {
-      --   "fc",
-      --   function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end,
-      --   desc = 'Search current word',
-      -- },
     }
   }
-  -- {
-  --   'dmtrKovalenko/fff.nvim',
-  --   build = function()
-  --     -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-  --     -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-  --     require("fff.download").download_or_build_binary()
-  --   end,
-  --   opts = {
-  --     debug = {
-  --       enabled = false,
-  --     },
-  --     prompt = ""
-  --   },
-  --   lazy = false,
-  --   keys = {
-  --     {
-  --       "<leader>p", -- try it if you didn't it is a banger keybinding for a picker
-  --       function() require('fff').find_files() end,
-  --       desc = 'FFFind files',
-  --     }
-  --   }
-  -- }
 }
